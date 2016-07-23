9cd0c9adf840c4e24bcd7134829a476c43cc03e5
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-07-23 13:47:43 +0200

    Assertion with nicer message.

---
+++ src/util/assert.js
@@ -0,0 +1,7 @@
+import assert from 'assert'
+
+const equal = (actual, expected, message = '') => assert.strictEqual(expected, actual,`${message} ${expected} !== ${actual}`);
+
+export default {
+    equal
+};
