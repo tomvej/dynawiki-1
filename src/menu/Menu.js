ba0fa8c78523fed46e99367d802ca4e641d70b74
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-08-07 14:19:30 +0200

    Menu keyboard listeners.

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -1,17 +1,53 @@
-import React from 'react'
+import React, {Children} from 'react'
 
 class Menu extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            selectedSection: null,
+            selectedItem: null
+        };
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     componentDidMount() {
-
+        window.addEventListener('keydown', this.handleKeyPress);
     }
 
     componentWillUnmount() {
+        window.removeEventListener('keydown', this.handleKeyPress);
+    }
 
+    handleKeyPress({key}) {
+        switch (key) {
+            case 'ArrowUp':
+                this.moveSelectionUp();
+                break;
+            case 'ArrowDown':
+                this.moveSelectionDown();
+                break;
+            case 'Enter':
+                break;
+            case 'Escape':
+                break;
+        }
+    }
+
+    moveSelectionUp() {
+        if (this.state.selectedSection) {
+            // select previous
+        } else {
+            //select last
+        }
+    }
+
+    moveSelectionDown() {
+        if (this.state.selectedSection) {
+            // select next
+        } else {
+            // select first
+        }
     }
 
     render() {
