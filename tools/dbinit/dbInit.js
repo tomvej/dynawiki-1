036fef106690cd46ded20ee68ca1e3dc32eff1e2
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-10-09 14:47:43 +0200

    Refactoring

--- tools/dbinit/dbInit.js
+++ tools/dbinit/dbInit.js
@@ -4,34 +4,35 @@ const clean = () => server.pagenode[GET_ALL]().then(
     (response) => Promise.all(response.map(({_id}) => server.pagenode[DELETE](_id)))
 );
 
-const pushNode = (node, idMap) => server.pagenode[CREATE]({
-    type: node.type,
-    text: node.text,
-    children: node.children.map((child) => idMap[child]),
-}).then(({_id}) => Object.assign({}, idMap, {[node.id]: _id}));
-
-const exit = (tree, id, idMap) => {
-    const parentId = tree[id].parent;
-    if (parentId || parentId === 0) {
-        const parent = tree[parentId];
-        const index = parent.children.indexOf(id);
-        if (index < parent.children.length - 1) {
-            visit(tree, parent.children[index + 1], idMap);
-        } else {
-            pushNode(parent, idMap).then((newIdMap) => exit(tree, parentId, newIdMap));
-        }
-    }
-};
-
-const visit = (tree, id, idMap) => {
-    const node = tree[id];
-    if (!node.children || !node.children.length) {
-        pushNode(node, idMap).then((newIdMap) => exit(tree, id, newIdMap));
-    } else {
-        visit(tree, node.children[0], idMap);
-    }
-};
-
 export default (tree) => {
-    clean().then(() => visit(tree, 0, {}));
+    const idMap = {};
+    const pushNode = (node) => server.pagenode[CREATE]({
+        type: node.type,
+        text: node.text,
+        children: node.children.map((child) => idMap[child]),
+    }).then(({_id}) => {
+        idMap[node.id] = _id;
+    });
+    const exit = (id) => {
+        const parentId = tree[id].parent;
+        if (parentId || parentId === 0) {
+            const parent = tree[parentId];
+            const index = parent.children.indexOf(id);
+            if (index < parent.children.length - 1) {
+                enter(parent.children[index + 1]);
+            } else {
+                pushNode(parent).then(() => exit(parentId));
+            }
+        }
+    };
+    const enter = (id) => {
+        const node = tree[id];
+        if (!node.children || !node.children.length) {
+            pushNode(node).then((newId) => exit(id));
+        } else {
+            enter(node.children[0]);
+        }
+    };
+
+    clean().then(() => enter(0));
 };
